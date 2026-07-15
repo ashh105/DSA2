e6c8a20cddc5d61040ae754757b9d9b873ab8ae1
aisha943 <aisha.comm18@gmail.com>
2026-07-15 19:37:04 +0500

    did some change

--- functions/palindrome.java
+++ functions/palindrome.java
@@ -7,18 +7,25 @@ public class palindrome {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a word");
         String word=input.nextLine();
-        System.out.println(ispalindrome(word));
+        if (ispalindrome(word)){
+            System.out.println(word+" is a palindrome");
+        }
+        else {
+            System.out.println(word+" is not a palindrome");
+        }
     }
-    static void ispalindrome(String word){
+    static boolean ispalindrome(String word){
         String reverse="";
       for (int i = word.length() -1; i >= 0; i--) {
           reverse = reverse + word.charAt(i);
-          if(word.equals(reverse)){
-              System.out.println("palindrome");
-          }
-          else {
-              System.out.println("not palindrome");
-          }
+
       }
+      if (word.equals(reverse)){
+          return true;
+
     }
+      else {
+          return false;
+      }
+}
 }
